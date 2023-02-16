27c56792cb5c1d5bf5fc62274f99ef189225b894
root <root@Rani.localdomain>
2023-02-16 09:45:21 +0100

    props names corrected

--- src/components/Quizzes.jsx
+++ src/components/Quizzes.jsx
@@ -1,9 +1,9 @@
-export default function Quizzes({questions, correct, incorrect}) {
+export default function Quizzes({allQuestions, allAnswer}) {
     return (
         <div className="quizzes-container">
             <div>
-                <h2>{questions}</h2>
-                <h5>{incorrect} {correct}</h5>
+                <h2>{allQuestions}</h2>
+                <h5>{allAnswer}</h5>
             </div>
         </div>
     )
